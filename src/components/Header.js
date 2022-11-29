52d16a08d5c54bfe405a39d83750bf77c980e1b7
Whit <whitneyharper1@aol.com>
2022-11-29 17:27:58 -0500

    Changed Nav.Links to Links and fixed styling of links.

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
@@ -11,10 +12,10 @@ const Header = () => {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ms-auto me-2">
-                    <Nav.Link href="/">Home</Nav.Link>
-                    <Nav.Link href="/products">Eyewear</Nav.Link>
-                    <Nav.Link href="/cart">Cart</Nav.Link>
-                    <Nav.Link href="/checkout">Checkout</Nav.Link>
+                    <Nav.Link><Link to="/">Home</Link></Nav.Link>
+                    <Nav.Link><Link to="/products">EyeWear</Link></Nav.Link>
+                    <Nav.Link><Link to="/cart">Cart</Link></Nav.Link>
+                    <Nav.Link><Link to="/checkout">Checkout</Link></Nav.Link>
                 </Nav>
                 </Navbar.Collapse>
             </Container>
