06c15587677b50c97d126eaff299044c80f76a62
Whit <whitneyharper1@aol.com>
2022-12-17 12:53:10 -0500

    Added Amplify file and code.

---
+++ amplify/backend/function/ordersFunctions/src/app.js
@@ -0,0 +1,113 @@
+/*
+Copyright 2017 - 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+    http://aws.amazon.com/apache2.0/
+or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and limitations under the License.
+*/
+
+
+
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
+
+// declare a new express app
+const app = express()
+app.use(bodyParser.json())
+app.use(awsServerlessExpressMiddleware.eventContext())
+
+// Enable CORS for all methods
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "*")
+  next()
+});
+
+const { v4: uuidv4 } = require("uuid");
+const AWS = require("aws-sdk");
+const documentClient = new AWS.DynamoDB.DocumentClient();
+
+const ORDER_TABLE = process.env.ORDER_TABLE;
+const ORDER_TYPE = "Order";
+const PRODUCT_ORDER_TABLE = process.env.PRODUCT_ORDER_TABLE;
+const PRODUCT_ORDER_TYPE = "ProductOrder";
+
+const createOrder = async (payload) => {
+  const { order_id, total } = payload;
+  var params = {
+    TableName: ORDER_TABLE,
+    Item: {
+      id: order_id,
+      __typename: ORDER_TYPE,
+      total: total,
+      updatedAt: new Date().toISOString(),
+      createdAt: new Date().toISOString()
+    }
+  };
+  console.log(params);
+  await documentClient.put(params).promise();
+};
+
+const createProductOrder = async (payload) => {
+  let productOrders = [];
+  for (let i = 0; i < payload.cart.length; i++) {
+    const cartItem = payload.cart[i];
+    productOrders.push({
+      PutRequest: {
+        Item: {
+          id: uuidv4(),
+          __typename: PRODUCT_ORDER_TYPE,
+          product_id: cartItem.id,
+          order_id: payload.order_id,
+          customer: payload.email,
+          createdAt: new Date().toISOString(),
+          updatedAt: new Date().toISOString()
+        }
+      }
+    });
+  }
+  let params = {
+    RequestItems: {}
+  };
+  params["RequestItems"][PRODUCT_ORDER_TABLE] = productOrders;
+  console.log(params);
+  await documentClient.batchWrite(params).promise();
+};
+
+/****************************
+* Example post method *
+****************************/
+
+app.post('/orders', async function(req, res) {
+  // Add your code here
+  try {
+    let payload = {...req.body,
+      order_id: uuidv4()}
+    
+    // create a new order
+    await createOrder(payload);
+
+    // links books with the order
+    await createProductOrder(payload);
+
+  
+    return res.status(200).json({ id: payload.order_id, message: "Success" });
+  } catch (err) {
+    console.log(err);
+    return new Error(err);
+  }
+});
+
+
+
+
+app.listen(3000, function() {
+    console.log("App started")
+});
+
+// Export the app object. When executing the application local this does nothing. However,
+// to port it to AWS Lambda we will create a wrapper around that will load the app from
+// this file
+module.exports = app
